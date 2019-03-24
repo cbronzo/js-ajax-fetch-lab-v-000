bfaa6502c39a6c83d2939b384f8931aea353c6ad
Catherine Bronzo <cbronzo@mac.com>
2019-03-24 02:01:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,16 @@ function forkRepo() {
 
 function showResults(json) {
   //use this function to display the results from forking via the API
+  
+  document.getElementById('results').innerHTML = `<a href=${json.html_url}>${
+    json.html_url
+  }</a>`;
 }
+
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
+  
 }
 
 function getIssues() {
