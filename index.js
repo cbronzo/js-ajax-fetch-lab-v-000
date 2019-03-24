a0fb17acafbab0e2913929fe3b9b18b3e2dfae0e
Catherine Bronzo <cbronzo@mac.com>
2019-03-24 02:03:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const baseURL = 'https://api.github.com';
-const user = 'cbronzo';
+
 
 function getToken() {
   //change to your token to run in browser, but set
@@ -37,12 +37,14 @@ function showResults(json) {
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
+  
   const repo = `${user}/js-ajax-fetch-lab`;
   const url = `${baseURL}/repos/${repo}/issues`;
   const postData = {
     title: document.getElementById('title').value,
     body: document.getElementById('body').value
   };
+  
    fetch(url, {
     method: 'POST',
     body: JSON.stringify(postData),
@@ -57,7 +59,7 @@ function createIssue() {
 function getIssues() {
   //once an issue is submitted, fetch all open issues to see the issues you are creating
   
-    const repo = `${user}/js-ajax-fetch-lab`;
+  const repo = `${user}/js-ajax-fetch-lab`;
   const url = `${baseURL}/repos/${repo}/issues`;
   fetch(url, {
     headers: {
